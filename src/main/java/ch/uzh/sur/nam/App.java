8b182b9fcc17f8c0369281c86188b5787189c1ed
Carmine Vassallo <carminevassallo@vpn-89-206-117-58.uzh.ch>
2016-11-19 12:08:16 +0100

    Initial commit

---
+++ src/main/java/ch/uzh/sur/nam/App.java
@@ -0,0 +1,13 @@
+package ch.uzh.sur.nam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
