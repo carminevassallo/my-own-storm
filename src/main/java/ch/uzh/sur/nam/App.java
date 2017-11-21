7de409d3af71e1042a37c3fb0662a18bc07391ca
Carmine Vassallo <vassallocarmine@gmail.com>
2017-11-21 14:58:17 +0100

    modified message

--- src/main/java/ch/uzh/sur/nam/App.java
+++ src/main/java/ch/uzh/sur/nam/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        String hellomessage = "Hello World!";
+        String hellomessage = "Hello World Travis!";
         System.out.println(hellomessage);
     }
 }
