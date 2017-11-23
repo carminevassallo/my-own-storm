fad3a70414821a3a4194ada117fba0de1fe0f190
Carmine Vassallo <vassallocarmine@gmail.com>
2017-11-23 10:04:02 +0100

    added minni

---
+++ src/main/java/ch/uzh/sur/nam/Minni.java
@@ -0,0 +1,12 @@
+package ch.uzh.sur.nam;
+
+public class Minni {
+
+	private Pippo pippo;
+	private Pluto pluto;
+	
+	public Minni(String name){
+		this.pippo = new Pippo(name);
+		this.pluto = new Pluto(name);
+	}
+}
