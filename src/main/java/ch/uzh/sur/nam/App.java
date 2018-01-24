40085fcd273b75edad8932fdff5a0efde3a5b9d4
Carmine Vassallo <vassallo@ifi.uzh.ch>
2018-01-24 11:29:30 +0100

    spaces

--- src/main/java/ch/uzh/sur/nam/App.java
+++ src/main/java/ch/uzh/sur/nam/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        String hellomessage = "Hello World!";
+        String hellomessage = "Hello W orld!";
         System.out.println(hellomessage);
        
         
