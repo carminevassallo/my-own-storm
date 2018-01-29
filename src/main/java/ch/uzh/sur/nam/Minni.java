46159829677d55f4d3afc9fe33efc07ea35b59ad
Carmine Vassallo <vassallo@ifi.uzh.ch>
2018-01-29 14:25:14 +0100

    [m] 4

--- src/main/java/ch/uzh/sur/nam/Minni.java
+++ src/main/java/ch/uzh/sur/nam/Minni.java
@@ -5,7 +5,7 @@ public class Minni {
 	private Pippo pippo;
 	private Pluto pluto;
 	
-	public Minni(String name){
+	public Minni(String name ){
 		this.pippo = new Pippo(name);
 		this.pluto = new Pluto(name);
 	}
